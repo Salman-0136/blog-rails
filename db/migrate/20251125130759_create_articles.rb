c7dcd89ea7453eb6c1cce521f237fe004cebf2d5
Salman Shahid <salmanshahid0136@gmail.com>
2025-11-25 18:35:31 +0500

    Add article model

--- db/migrate/20251125130759_create_articles.rb
+++ db/migrate/20251125130759_create_articles.rb
@@ -1,8 +1,14 @@
 class CreateArticles < ActiveRecord::Migration[7.2]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :description
+      t.string :title, null: false
+      t.text :description, null: false
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.string :status, default: "draft"
+      t.datetime :published_at
+      t.string :cover_image
+      t.integer :views_count, default: 0
+
       t.timestamps
     end
   end
